ad7e2599170b6b05bc21ab24dc10f3e45d20e21c
yamachu <yamachu.dev@gmail.com>
2024-05-02 19:52:31 +0900

    Format

--- src/postPreTestActionUrl.js
+++ src/postPreTestActionUrl.js
@@ -10,6 +10,7 @@ Testing: https://github.com/${owner}/${repo}/actions/runs/${runId}
 You can view state by GitHub CLI below command:
   - gh run view --repo ${owner}/${repo} ${runId}
   - gh run watch --repo ${owner}/${repo} ${runId}
+
 If you want to cancel the test, you can cancel it by GitHub CLI below command:
   - gh run cancel --repo ${owner}/${repo} ${runId}
 `;
